82545edab63e502c2ce879f542bc0235c29a9c93
Saher Zaid <Saher.zaid@iths.se>
2023-09-17 17:20:29 +0200

    Add project files.

---
+++ LAPP 1/Program.cs
@@ -0,0 +1,61 @@
+﻿
+            Console.Write("Input: ");
+            string input = Console.ReadLine();
+            string bold = "";
+            int count = 0;
+            long sum = 0;
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+
+                bold = "";
+
+                if (Char.IsDigit(input[i]))
+                {
+                    bold += input[i];
+                    for (int j = i + 1; j < input.Length; j++)
+                    {
+                        if (Char.IsDigit(input[j]))
+                        {
+                            bold += input[j];
+                            if (input[i] == input[j])
+                            {
+                                count++;
+                                sum += Convert.ToInt64(bold);
+                                Console.ResetColor();
+                                Console.Write(input.Substring(0, i));
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.Write(bold);
+                                Console.ResetColor();
+                                Console.WriteLine(input.Substring(j + 1, (input.Length - (j + 1))));
+
+                                bold = "";
+                                j = input.Length;
+                            }
+
+                        }
+                        else
+                        {
+                            bold = "";
+                            j = input.Length;
+
+
+                        }
+
+
+                    }
+
+                }
+                else
+                {
+                    //bold = "";
+                    continue;
+
+                }
+
+            }
+            Console.WriteLine("Count : " + count);
+            Console.WriteLine("Sum : " + sum);
+  
